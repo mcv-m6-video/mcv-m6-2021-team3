7879af0019881e021f7ea72a23cac60b69db2e4a
gemmaalaix <gemma.alaix97@gmail.com>
2021-03-10 01:13:19 +0100

    results2

--- Week1/main.py
+++ Week1/main.py
@@ -8,13 +8,14 @@ display = True
 noise_evaluation = True
 
 path = '../'
-visualization = 'base'
+visualization = 'color_wheel'
 
 
 def main(argv):
     if len(argv) > 1:
         task = float(argv[1])
     else:
+        task = 1
 
     if int(task) in [1, 2]:
         data_dir = join(path, 'AICity_data/train/S03/c010')
