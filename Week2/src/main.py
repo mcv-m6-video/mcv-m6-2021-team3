ed09e477ad335b72b8b828d4e08633f508090ec0
brugues <bruguesjosep@outlook.com>
2021-03-14 17:16:21 +0100

    create options dict

--- Week2/src/main.py
+++ Week2/src/main.py
@@ -9,7 +9,8 @@ debug = True
 test_mode = False
 resize_factor = 0.5
 method = 'GMG'
-colorspace = "HSV"
+colorspace = "LAB"
+
 
 def main(argv):
     if len(argv) > 1:
@@ -17,16 +18,52 @@ def main(argv):
     else:
         task = 1.1
 
+
     if int(task) == 1:
         frames_paths = join(data_path, 'AICity/train/S03/c010/vdo')
 
         if task == 1.1:
-            aicity = AICity(frames_paths, resize_factor=resize_factor, task=task, test_mode=test_mode, laplacian=True)
+            options = {
+                'resize_factor': 0.5,
+                'denoise': False,
+                'split_factor': 0.25,
+                'test_mode': True,
+                'colorspace': 'gray',
+                'extension': 'png',
+                'laplacian': True,
+                'median_filter': False,
+                'pre_denoise': False,
+                'alpha': 3,
+                'rho': 0.5,
+                'rm_noise': None,
+                'fill': False,
+                'adaptive_model': False,
+                'task': task
+            }
+
+            aicity = AICity(frames_paths, options)
             aicity.create_background_model()
             aicity.get_frames_background()
         elif task == 1.2:
-            aicity = AICity(frames_paths, resize_factor=resize_factor, task=task, test_mode=test_mode,
-                            rm_noise=True, fill=True, noise_opening=True, pre_denoise=True, laplacian=True, alpha=5)
+            options = {
+                'resize_factor': 0.5,
+                'denoise': False,
+                'split_factor': 0.25,
+                'test_mode': True,
+                'colorspace': 'gray',
+                'extension': 'png',
+                'laplacian': False,
+                'median_filter': False,
+                'pre_denoise': False,
+                'alpha': 3,
+                'rho': 0.5,
+                'rm_noise': None,
+                'fill': False,
+                'adaptive_model': False,
+                'task': task
+            }
+
+            aicity = AICity(frames_paths, options)
             aicity.create_background_model()
             aicity.get_frames_background()
 
@@ -34,53 +71,92 @@ def main(argv):
         frames_paths = join(data_path, 'AICity/train/S03/c010/vdo')
 
         if task == 2.1:
-            aicity = AICity(frames_paths, resize_factor=resize_factor, task=task, test_mode=test_mode,
-                            adaptative_model=True, alpha=2, rho=0.01)
+            options = {
+                'resize_factor': 0.5,
+                'denoise': False,
+                'split_factor': 0.25,
+                'test_mode': True,
+                'colorspace': 'gray',
+                'extension': 'png',
+                'laplacian': False,
+                'median_filter': False,
+                'pre_denoise': False,
+                'alpha': 3,
+                'rho': 0.5,
+                'rm_noise': None,
+                'fill': False,
+                'adaptive_model': False,
+                'task': task
+            }
+
+            aicity = AICity(frames_paths, options)
             aicity.create_background_model()
             aicity.get_frames_background()
-        elif task == 1.2:
-            pass
 
     elif int(task) == 3:
+        options = {
+            'resize_factor': 0.5,
+            'denoise': False,
+            'split_factor': 0.25,
+            'test_mode': True,
+            'colorspace': 'gray',
+            'extension': 'png',
+            'laplacian': False,
+            'median_filter': False,
+            'pre_denoise': False,
+            'alpha': 3,
+            'rho': 0.5,
+            'rm_noise': None,
+            'fill': False,
+            'adaptive_model': False,
+            'task': task
+        }
+
         frames_paths = join(data_path, 'AICity/train/S03/c010/vdo')
-        aicity = AICity(frames_paths, resize_factor=0.5, split_factor=1, pre_denoise=False, laplacian=True)
+        aicity = AICity(frames_paths, options)
         frames = aicity.read_frames()
+
         if method == 'MOG2':
             backSub = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
-            for frame in frames:
-                bg_fg_MOG2 = backSub.apply(frame)
-                bg_MOG2 = backSub.getBackgroundImage()
-                bg_fg_MOG2[bg_fg_MOG2 != 255] = 0
-                cv2.imshow('Background', bg_fg_MOG2)
-                cv2.waitKey(100)
+            # bg_MOG2 = backSub.getBackgroundImage()
+            s
         elif method == 'KNN':
             backSub = cv2.createBackgroundSubtractorKNN(detectShadows=False)
-            for frame in frames:
-                bg_fg_KNN = backSub.apply(frame)
-                bg_fg_KNN[bg_fg_KNN != 255] = 0
-                cv2.imshow('Background', bg_fg_KNN)
-                cv2.waitKey(100)
+
         elif method == 'GMG':
             backSub = cv2.bgsegm.createBackgroundSubtractorGMG()
-            for frame in frames:
-                bg_fg_GMG = backSub.apply(frame)
-                bg_fg_GMG[bg_fg_GMG != 255] = 0
-                cv2.imshow('Background', bg_fg_GMG)
-                cv2.waitKey(100)
+
         elif method == 'LSBP':
             backSub = cv2.bgsegm.createBackgroundSubtractorLSBP()
-            for frame in frames:
-                bg_fg_LSBP = backSub.apply(frame)
-                bg_fg_LSBP[bg_fg_LSBP != 255] = 0
-                cv2.imshow('Background', bg_fg_LSBP)
-                cv2.waitKey(100)
+
+        for frame in frames:
+            bg_fg_KNN = backSub.apply(frame)
+            bg_fg_KNN[bg_fg_KNN != 255] = 0
+            cv2.imshow('Background', bg_fg_KNN)
+            cv2.waitKey(100)
 
     elif int(task) == 4:
         frames_paths = join(data_path, 'AICity/train/S03/c010/vdo')
-        adaptative = False
 
-        aicity = AICity(frames_paths, resize_factor=0.5, test_mode=test_mode, grayscale=False, alpha=2,
-                        adaptative_model=adaptative, colorspace=colorspace, pre_denoise=False, laplacian=False)
+        options = {
+            'resize_factor': 0.5,
+            'denoise': False,
+            'split_factor': 0.25,
+            'test_mode': True,
+            'colorspace': 'gray',
+            'extension': 'png',
+            'laplacian': False,
+            'median_filter': False,
+            'pre_denoise': False,
+            'alpha': 3,
+            'rho': 0.5,
+            'rm_noise': None,
+            'fill': False,
+            'adaptive_model': False,
+            'task': task
+        }
+
+        aicity = AICity(frames_paths, options)
         aicity.create_background_model()
         aicity.get_frames_background()
 
--- Week2/src/utils/ai_city.py
+++ Week2/src/utils/ai_city.py
@@ -2,12 +2,7 @@ import numpy as np
 import cv2
 from os.path import join
 import glob
-import matplotlib.pyplot as plt
-import time
 from tqdm import tqdm
-
-# Deep Learning Background substraction
-import tensorflow as tf
 
 
 def filter_noise(bg, post_processing=True, min_area=0.15):
@@ -17,33 +12,34 @@ def filter_noise(bg, post_processing=True, min_area=0.15):
 
     if post_processing:
         # Opening
-        bg = cv2.morphologyEx(bg, cv2.MORPH_OPEN,  np.ones((1,1)))  
+        bg = cv2.morphologyEx(bg, cv2.MORPH_OPEN, np.ones((1, 1)))
         # Closing
-        bg = cv2.morphologyEx(bg, cv2.MORPH_CLOSE, np.ones((3,3)))
+        bg = cv2.morphologyEx(bg, cv2.MORPH_CLOSE, np.ones((3, 3)))
 
     # Connected components
     num_lab, labels = cv2.connectedComponents(bg)
 
     # Filter by area
-    rm_labels = [u for u in np.unique(labels) if np.sum(labels == u) < min_area*len(bg)]
+    rm_labels = [u for u in np.unique(labels) if np.sum(labels == u) < min_area * len(bg)]
     for label in rm_labels:
         bg[np.where(labels == label)] = 0
-        labels[np.where(labels == label)] = 0    
-    
+        labels[np.where(labels == label)] = 0
+
     return bg, labels
+
 
 def generate_bbox(mask):
     """
     
     """
-    pos = np.where(mask==255)
+    pos = np.where(mask == 255)
 
     y = np.min(pos[0])
     x = np.min(pos[1])
     h = np.max(pos[0]) - np.min(pos[0])
     w = np.max(pos[1]) - np.min(pos[1])
 
-    return x,y,w,h
+    return x, y, w, h
 
 
 def fill_gaps(labels):
@@ -51,30 +47,30 @@ def fill_gaps(labels):
 
     """
     bg_idx = np.argmax([np.sum(labels == u) for u in np.unique(labels)])
-    fg_idx = np.delete(np.unique(labels),bg_idx)
-    
+    fg_idx = np.delete(np.unique(labels), bg_idx)
+
     bg = np.zeros(labels.shape, dtype=np.uint8)
 
     for label in fg_idx:
         aux = np.zeros(labels.shape, dtype=np.uint8)
-        aux[labels==label]=255
+        aux[labels == label] = 255
 
-        contours,_ = cv2.findContours(aux, 1, 2)
+        contours, _ = cv2.findContours(aux, 1, 2)
 
         # create hull array for convex hull points
         hull = []
         # calculate points for each contour
         for i in range(len(contours)):
             # creating convex hull object for each contour
-            #hull = cv2.convexHull(contours[i], False)
-            epsilon = 0.1*cv2.arcLength(contours[i],True)
-            approx = cv2.approxPolyDP(contours[i],epsilon,True)
-            aux = cv2.fillPoly(aux, pts=[approx],color=(255))
+            # hull = cv2.convexHull(contours[i], False)
+            epsilon = 0.1 * cv2.arcLength(contours[i], True)
+            approx = cv2.approxPolyDP(contours[i], epsilon, True)
+            aux = cv2.fillPoly(aux, pts=[approx], color=(255))
 
-        x,y,w,h = generate_bbox(aux)
-        if np.sum(aux>0)/(w*h) > .35:
-            aux = cv2.rectangle(aux,(x,y),(x+w,y+h),(50),2)
-            bg = bg+aux
+        x, y, w, h = generate_bbox(aux)
+        if np.sum(aux > 0) / (w * h) > .35:
+            aux = cv2.rectangle(aux, (x, y), (x + w, y + h), (50), 2)
+            bg = bg + aux
 
         '''drawing = np.zeros((aux.shape[0], aux.shape[1], 3), np.uint8)
         # draw contours and hull points
@@ -85,8 +81,9 @@ def fill_gaps(labels):
             cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
             # draw ith convex hull object
             cv2.drawContours(drawing, hull, i, color, 1, 8)'''
-    
+
     return bg
+
 
 def get_single_objs(bg):
     """
@@ -97,7 +94,7 @@ def get_single_objs(bg):
 
     # Generate bboxes    
     bg = fill_gaps(labels)
-    
+
     # Filter by aspect ratio
     return bg
 
@@ -107,31 +104,16 @@ class AICity:
 
     """
 
-    def __init__(self, data_path, test_mode=False, resize_factor=0.5, denoise=False, split_factor=0.15,
-                 grayscale=True, colorspace="LAB", extension="png", laplacian=False, pre_denoise=False, task=1.1,
-                 alpha=3, rho=0.5, rm_noise=None, fill=False, adaptative_model=False):
+    def __init__(self, data_path, options):
         """
 
         """
 
         # PARAMETERS
         self.data_path = data_path
-        self.frames_paths = glob.glob(join(self.data_path, "*." + extension))
+        self.frames_paths = glob.glob(join(self.data_path, "*." + options['extension']))
         self.frames_paths.sort()
-        self.resize_factor = resize_factor
-        self.denoise = denoise
-        self.split_factor = split_factor
-        self.grayscale = grayscale
-        self.colorspace = colorspace
-        self.laplacian = laplacian
-        self.pre_denoise = pre_denoise
-        self.task = task
-        self.alpha = alpha
-        self.rho = rho
-        self.rm_noise = rm_noise
-        self.fill = fill
-        self.test_mode = test_mode
-        self.adaptative_model = adaptative_model
+        self.options = options
 
         # FUNCTIONS
         self.split_data()
@@ -140,12 +122,12 @@ class AICity:
         """
 
         """
-        if self.test_mode:
-            self.frames_paths = self.frames_paths[0:int(len(self.frames_paths)/10)]
-            
+        if self.options['test_mode']:
+            self.frames_paths = self.frames_paths[0:int(len(self.frames_paths) / 10)]
+
         self.bg_modeling_frames_paths = self.frames_paths[
-                                        :int(len(self.frames_paths) * self.split_factor)]  # 535 frames
-        self.bg_frames_paths = self.frames_paths[int(len(self.frames_paths) * self.split_factor):]
+                                        :int(len(self.frames_paths) * self.options['split_factor'])]  # 535 frames
+        self.bg_frames_paths = self.frames_paths[int(len(self.frames_paths) * self.options['split_factor']):]
         # 1606 frames
 
     def create_background_model(self):
@@ -154,7 +136,7 @@ class AICity:
         """
         bg_modeling_frames = self.read_frames()
 
-        if self.grayscale:
+        if self.options['colorspace'] == 'gray':
             n_frames, height, width = bg_modeling_frames.shape
             gaussian = np.zeros((height, width, 2))
             gaussian[:, :, 0] = np.mean(bg_modeling_frames, axis=0)
@@ -165,7 +147,7 @@ class AICity:
             self.background_model = gaussian
 
         else:
-            if self.colorspace == "LAB":
+            if self.options['colorspace'] == "LAB":
                 n_frames, height, width, n_channels = bg_modeling_frames.shape
                 gaussian = np.zeros((height, width, 4))
 
@@ -179,7 +161,7 @@ class AICity:
 
                 self.background_model = gaussian
 
-            elif self.colorspace == "HSV":
+            elif self.options['colorspace'] == "HSV":
                 n_frames, height, width = bg_modeling_frames.shape
 
                 gaussian = np.zeros((height, width, 2))
@@ -193,21 +175,17 @@ class AICity:
     def get_frame_background(self, frame):
         """
         :param frame:
-        :param model:
-        :param grayscale:
-        :param rm_noise:
-        :param fill:
         :return:
         """
 
-        if self.grayscale:
+        if self.options['colorspace'] == 'gray':
             bg = np.zeros_like(frame)
 
             diff = frame - self.background_model[:, :, 0]
-            foreground_idx = np.where(abs(diff) > self.alpha * (2 + self.background_model[:, :, 1]))
+            foreground_idx = np.where(abs(diff) > self.options['alpha'] * (2 + self.background_model[:, :, 1]))
 
             bg[foreground_idx[0], foreground_idx[1]] = 255
-            
+
             bg = get_single_objs(bg)
 
             '''if self.rm_noise is not None:
@@ -215,24 +193,24 @@ class AICity:
             if self.fill:
                 bg = fill_gaps(bg)'''
         else:
-            if self.colorspace == "LAB" or self.colorspace == "YCbCr":
+            if self.options['colorspace'] == "LAB" or self.options['colorspace'] == "YCbCr":
                 bg = np.zeros((frame.shape[0], frame.shape[1]))
 
                 diff_ch1 = frame[:, :, 0] - self.background_model[:, :, 0]
                 diff_ch2 = frame[:, :, 1] - self.background_model[:, :, 2]
 
-                foreground_ch1_idx = np.where(abs(diff_ch1) > self.alpha * (2 + self.background_model[:, :, 1]))
-                foreground_ch2_idx = np.where(abs(diff_ch2) > self.alpha * (2 + self.background_model[:, :, 3]))
+                foreground_ch1_idx = np.where(abs(diff_ch1) > self.options['alpha'] * (2 + self.background_model[:, :, 1]))
+                foreground_ch2_idx = np.where(abs(diff_ch2) > self.options['alpha'] * (2 + self.background_model[:, :, 3]))
 
                 bg[foreground_ch1_idx[0], foreground_ch1_idx[1]] = 255
                 bg[foreground_ch2_idx[0], foreground_ch2_idx[1]] = 255
 
-            elif self.colorspace == "HSV":
+            elif self.options['colorspace'] == "HSV":
                 bg = np.zeros_like(frame)
 
                 diff = frame - self.background_model[:, :, 0]
 
-                foreground_idx = np.where(abs(diff) > self.alpha * (2 + self.background_model[:, :, 1]))
+                foreground_idx = np.where(abs(diff) > self.options['alpha'] * (2 + self.background_model[:, :, 1]))
 
                 bg[foreground_idx[0], foreground_idx[1]] = 255
 
@@ -240,83 +218,94 @@ class AICity:
 
     def get_frames_background(self):
         for frame_id, frame_path in tqdm(enumerate(self.bg_frames_paths), 'Predicting background'):
-            frame = self.read_frame(frame_path, grayscale=self.grayscale, laplacian=self.laplacian, pre_denoise=self.pre_denoise)
-                        
-            bg = self.get_frame_background(frame)                              
-            img = self.read_frame(frame_path, grayscale=True)
+            frame = self.read_frame(frame_path, colorspace=self.options['colorspace'],
+                                    laplacian=self.options['laplacian'], pre_denoise=self.options['pre_denoise'])
+
+            bg = self.get_frame_background(frame)
+            img = self.read_frame(frame_path, colorspace='gray')
             img = cv2.hconcat((bg, img.astype(np.uint8)))
-            #cv2.imwrite('laplacian/{}.jpg'.format(frame_id),img)
+            # cv2.imwrite('laplacian/{}.jpg'.format(frame_id),img)
             cv2.imshow("Background", img)
             cv2.waitKey(100)
 
-            if self.adaptative_model:
+            if self.options['adaptive_model']:
                 self.update_gaussian(frame, bg)
-            
+
             frame, img, bg = None, None, None
 
     def read_frames(self):
         """
-        :param paths:
-        :param grayscale:
         :return:
         """
 
         images = []
         for file_name in tqdm(self.bg_modeling_frames_paths, 'Reading frames'):
-            images.append(self.read_frame(file_name, grayscale=self.grayscale, laplacian=self.laplacian,
-                                          pre_denoise=self.pre_denoise))
-        
+            images.append(self.read_frame(file_name, colorspace=self.options['colorspace'],
+                                          laplacian=self.options['laplacian'],
+                                          pre_denoise=self.options['pre_denoise']))
+
         return np.asarray(images)
 
-    def read_frame(self, path, grayscale=True, laplacian=False, pre_denoise=False):
+    def read_frame(self, path, colorspace='gray', laplacian=False, pre_denoise=False):
         """
 
         """
 
-        if grayscale:
+        if colorspace == 'gray':
             image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            if self.resize_factor < 1.0:
-                image = cv2.resize(image,
-                                   (int(image.shape[1] * self.resize_factor), int(image.shape[0] * self.resize_factor)),
-                                   cv2.INTER_CUBIC)
-            if pre_denoise:   
-                image = cv2.fastNlMeansDenoising(image, templateWindowSize = 7)
-            if laplacian:
-                image = cv2.Laplacian(image, cv2.CV_8U)
-        else:
-            image = cv2.imread(path)
 
-            if self.resize_factor < 1.0:
+            if self.options['resize_factor'] < 1.0:
                 image = cv2.resize(image,
-                                   (int(image.shape[1] * self.resize_factor), int(image.shape[0] * self.resize_factor)),
+                                   (int(image.shape[1] * self.options['resize_factor']),
+                                    int(image.shape[0] * self.options['resize_factor'])),
                                    cv2.INTER_CUBIC)
             if pre_denoise:
                 image = cv2.fastNlMeansDenoising(image, templateWindowSize=7)
             if laplacian:
                 image = cv2.Laplacian(image, cv2.CV_8U)
 
-            if self.colorspace == "LAB":
+        else:
+            image = cv2.imread(path)
+
+            if self.options['resize_factor'] < 1.0:
+                image = cv2.resize(image,
+                                   (int(image.shape[1] * self.options['resize_factor']),
+                                    int(image.shape[0] * self.options['resize_factor'])),
+                                   cv2.INTER_CUBIC)
+            if pre_denoise:
+                image = cv2.fastNlMeansDenoising(image, templateWindowSize=7)
+            if laplacian:
+                image = cv2.Laplacian(image, cv2.CV_8U)
+
+            if self.options['colorspace'] == "LAB":
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)[:, :, 1:]
 
-            if self.colorspace == "YCbCr":
+            elif self.options['colorspace'] == "YCbCr":
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)[:, :, 1:]
 
-            if self.colorspace == "HSV":
+            elif self.options['colorspace'] == "HSV":
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[:, :, 0]
+
+            if self.options['median_filter']:
+                filter_size = 5
+                kernel = np.ones((filter_size, filter_size)) / filter_size ** 2
+                image[:, :, 0] = cv2.bilateralFilter(image[:, :, 0], 9, 75, 75)
+                image[:, :, 1] = cv2.bilateralFilter(image[:, :, 1], 9, 75, 75)
+                # image = cv2.filter2D(image, -1, kernel)
 
         return image
 
-    
     def update_gaussian(self, frame, bg):
         """
 
         """
 
-        if self.grayscale:
-            [x,y] = np.where(bg==0)
-            #update mean
-            self.background_model[x, y, 0] = self.rho*frame[x,y] + (1-self.rho)*self.background_model[x, y, 0]
-            #update std
-            self.background_model[x, y, 1] = self.rho*np.square(frame[x,y] - self.background_model[x, y, 0]) + (1-self.rho)*self.background_model[x, y, 1]
+        if self.options['colorspace'] == 'gray':
+            [x, y] = np.where(bg == 0)
+            # update mean
+            self.background_model[x, y, 0] = self.options['rho'] * frame[x, y] + (1 - self.options['rho']) * self.background_model[x, y, 0]
+            # update std
+            self.background_model[x, y, 1] = self.options['rho'] * np.square(frame[x, y] - self.background_model[x, y, 0]) + (
+                        1 - self.options['rho']) * self.background_model[x, y, 1]
         else:
             pass
