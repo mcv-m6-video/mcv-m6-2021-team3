8cfd3b9aff44af8f7f33fb970a4b34d1f73e242c
gemmaalaix <gemma.alaix97@gmail.com>
2021-03-20 13:53:21 +0100

    re-organize tfm

--- Week3/src/main.py
+++ Week3/src/main.py
@@ -1,9 +1,9 @@
 import os
 from os.path import join
-#from utils.ai_city import AICity
+from utils.ai_city import AICity
 from utils.utils import write_json_file
 from config.config import Config
-#import utils.detect2 as detect2
+import utils.detect2 as detect2
 import utils.tf_models as tfm
 
 
@@ -11,9 +11,9 @@ def main(args):
 
     os.makedirs(join(args.output_path, str(args.task)), exist_ok=True)
 
-    # aicity = AICity(args)
-    # aicity.train_val_split()
-    # aicity.data_to_model()
+    #aicity = AICity(args)
+    #aicity.train_val_split()
+    #aicity.data_to_model()
 
     #img_path = aicity.frames_paths[0]
     #print(detect2.predict(img_path, 'faster_rcnn'))
